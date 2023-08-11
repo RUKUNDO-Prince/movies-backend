efd00ef44edeffb19626fd26239e3832b0f7d276
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-08-11 22:34:48 +0300

    movies app backend

--- src/main/java/com/princerukundo/moviesbackend/ReviewController.java
+++ src/main/java/com/princerukundo/moviesbackend/ReviewController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/vi/reviews")
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
